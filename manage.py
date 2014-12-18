f0a6a063e1e4fe9fa5f091a84ae7d2848aa54c96
jimchim <jim@getalogue.com>
2014-12-19 06:57:14 +0800

    added boostrap, figured out how staticfile works in django

--- manage.py
+++ manage.py
@@ -2,6 +2,14 @@
 import os
 import sys
 
+#django_base_path = os.path.dirname(__file__) 
+#print os.path.join(django_base_path, 'static')
+
+# reminder for myself
+# django is ran from manage.py, not settings.py
+# so os.path.dirname(__file__) would return the path of manage.py
+
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "polling.settings")
 
